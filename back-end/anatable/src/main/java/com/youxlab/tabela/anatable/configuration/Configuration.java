97706cb87cf7e72e7b306c45f76c980fa69557e9
julincesar02 <juliogoiabinha060@gmail.com>
2025-09-05 06:07:31 -0300

    classe para ajustes de ligação entre front e back

--- back-end/anatable/src/main/java/com/youxlab/tabela/anatable/configuration/Configuration.java
+++ back-end/anatable/src/main/java/com/youxlab/tabela/anatable/configuration/Configuration.java
@@ -1,14 +1,11 @@
 package com.youxlab.tabela.anatable.configuration;
 
-import org.springframework.context.annotation.Configuration;
-
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class configuration {
+@org.springframework.context.annotation.Configuration
+public class Configuration {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
